f8d012c976fc3a48a85418bb27a63455d5f0ccab
Jambrose <ambrose_john@yahoo.com>
2021-04-16 22:28:24 -0500

    set initial values and use recude function CORRECTLY

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -1,6 +1,38 @@
 function checkCashRegister(price, cash, cid) {
-    var change;
+    let change = [];
+    let currencyDict = {
+        PENNY: 0.01,
+        NICKEL: 0.05,
+        DIME: 0.1,
+        QUARTER: 0.25,
+        ONE: 1,
+        FIVE: 5,
+        TEN: 10,
+        TWENTY: 20,
+    };
+    // unpack the cid into till
+    let till = cid.splice('');
+
+    // get till total
+    let tillTotal = till.reduce((sum, currency) => {
+        return sum + currency[1];
+    }, 0);
+
+    //round that stuff down
+    tillTotal = tillTotal.toFixed(2);
+
+    let statusAndChange = {};
     return change;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [
+    ['PENNY', 1.01],
+    ['NICKEL', 2.05],
+    ['DIME', 3.1],
+    ['QUARTER', 4.25],
+    ['ONE', 90],
+    ['FIVE', 55],
+    ['TEN', 20],
+    ['TWENTY', 60],
+    ['ONE HUNDRED', 100],
+]);
